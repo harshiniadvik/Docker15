167a95085b4c808be7a18267d27d96438bfb9cde
harshiniadvik <v.harshini1817@gmail.com>
2025-07-15 14:05:09 +0530

    adding two files

--- samplename.java
+++ samplename.java
@@ -1,4 +1,9 @@
-FROM openjdk:17
-COPY samplename.java /samplename.java
-RUN javac samplename.java
-CMD ["java", "samplename"]
+public class samplename {
+    public static void main(String[] args) {
+        // Declare a variable to hold the name
+        String name = "John Doe";
+        
+        // Print the name to the console
+        System.out.println("Hello, my name is " + name);
+    }
+}
